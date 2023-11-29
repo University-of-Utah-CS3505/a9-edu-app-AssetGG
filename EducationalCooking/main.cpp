6b6bd86ad3783ac02678de5e0e75280a83e9858f
David Cosby <davjcosby@gmail.com>
2023-11-28 20:11:02 -0700

    got rid of demo code

--- EducationalCooking/main.cpp
+++ EducationalCooking/main.cpp
@@ -9,20 +9,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     PlayerModel m;
     PlayerView v;
+    Physics physics;
     Controller c(m, v);
-
-    Physics physics(15.0);
-
-    auto box = Physics::createBoxShape(1.0, 1.0);
-    physics.registerStaticObject("wall", &box, 0.0, 50.0);
-
-    auto circle = Physics::createCircleShape(1.0);
-    auto tomato = physics.registerDynamicObject("tomato", &circle, 0.0, 0.0);
-
-    tomato.body->SetLinearVelocity(b2Vec2(0.0, 12.0));
 
     v.show();
     physics.start();
-
     return a.exec();
 }
