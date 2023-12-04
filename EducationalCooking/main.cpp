01cc559c4602cbc78d79ebd6f6350bddfb97f300
David Cosby <davjcosby@gmail.com>
2023-12-04 16:29:15 -0700

    removed testRecipe from main()

--- EducationalCooking/main.cpp
+++ EducationalCooking/main.cpp
@@ -4,35 +4,6 @@
 #include "playermodel.h"
 #include "playerview.h"
 #include "startupscreen.h"
-#include "comparisoningredient.h"
-#include "tools.h"
-
-Recipe testRecipe()
-{
-    std::vector<ComparisonIngredient> base;
-    std::vector<ComparisonIngredient> bonus;
-    std::vector<Ingredient> available;
-
-    available.push_back(Ingredient("tomato",
-                                   QImage(":/sprites/Sprites/Tomato.png"),
-                                   QImage(":/sprites/Sprites/Tomato.png"),
-                                   QImage(":/sprites/Sprites/Tomato.png"),
-                                   true,
-                                   true,
-                                   240,
-                                   240));
-    available.push_back(Ingredient("fishy",
-                                   QImage(":/sprites/Sprites/Mackarel.png"),
-                                   QImage(":/sprites/Sprites/Mackarel.png"),
-                                   QImage(":/sprites/Sprites/Mackarel.png"),
-                                   true,
-                                   true,
-                                   200,
-                                   240,
-                                   3.0));
-
-    return Recipe("testRecipe", base, bonus, available, QImage());
-}
 
 int main(int argc, char *argv[])
 {
@@ -41,8 +12,6 @@ int main(int argc, char *argv[])
     PlayerView playerView;
     Physics physics;
     PlayerModel model(physics); // Instance of PlayerModel
-
-    Recipe recipe = testRecipe();
 
     Controller c(model, playerView);
 
