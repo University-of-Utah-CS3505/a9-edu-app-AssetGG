fceb32a8c15a0ffee6877f809ac30161cf55cd7a
PluggedInGamer <kaz861@gmail.com>
2023-12-07 18:30:54 -0700

    Added comments to ComparisonIngredient

--- EducationalCooking/comparisoningredient.cpp
+++ EducationalCooking/comparisoningredient.cpp
@@ -1,6 +1,7 @@
 #include "comparisoningredient.h"
 #include "ingredient.h"
 
+// Constructor for ComparisonIngredient.
 ComparisonIngredient::ComparisonIngredient(std::string foodName, bool isCut, bool isCooked)
 {
     cut = isCut;
@@ -8,6 +9,7 @@ ComparisonIngredient::ComparisonIngredient(std::string foodName, bool isCut, boo
     name = foodName;
 }
 
+// Default Constructor
 ComparisonIngredient::ComparisonIngredient()
 {
     cut = false;
@@ -15,19 +17,24 @@ ComparisonIngredient::ComparisonIngredient()
     name = "default";
 }
 
+// Getter for the name of ComparisonIngredient.
 std::string ComparisonIngredient::GetName() const {
     return name;
 }
 
+// Getter for cooked of ComparisonIngredient
 bool ComparisonIngredient::IsCooked() const
 {
     return cooked;
 }
+
+// Getter for the cut of ComparisonIngrdient
 bool ComparisonIngredient::IsCut() const
 {
     return cut;
 }
 
+// Compares the state of ingredient sent in to this ComparisonIngredient's state.
 bool ComparisonIngredient::legalIngredient(Ingredient ingredient)
 {
     if(ingredient.IsCut() != cut || ingredient.IsCooked() != cooked)
