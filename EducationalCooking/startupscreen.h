3ab709696e4e8b0bddec71ad88011530def8a1fe
Yukon Z <yz768737@gmail.com>
2023-12-07 21:44:29 -0700

    Header Comments

--- EducationalCooking/startupscreen.h
+++ EducationalCooking/startupscreen.h
@@ -14,20 +14,36 @@ class StartupScreen : public QWidget {
     Q_OBJECT
 
 public:
+    // Constructor
     StartupScreen(PlayerModel &model, PlayerView &view, QWidget *parent = nullptr);
+
+    // Destructor
     ~StartupScreen();
 
 private slots:
+    // Slot for handling the click event on a recipe card
     void onRecipeCardClicked(int recipeIndex);
+
+    // Slot for handling the click event on a specific recipe card
     void handleRecipeCardClicked(const QString &recipeName);
+
+    // Slot for handling the click event on the "Learn Recipe" button
     void onLearnRecipeClicked();
 
 private:
+    // Vector to store recipe card widgets
     QVector<RecipeCardWidget*> recipeCards;
+
+    // User interface object
     Ui::StartupScreen *ui;
+
+    // Reference to the player model
     PlayerModel& playerModel;
+
+    // Reference to the player view
     PlayerView &playerView;
 
+    // Images for recipe cards
     QImage recipeCardImage1;
     QImage recipeCardImage2;
     QImage recipeCardImage3;
@@ -35,8 +51,13 @@ private:
     QImage recipeCardImage5;
     QImage recipeCardImage6;
 
+    // Method to create recipe card widgets
     void createRecipeCards();
+
+    // Method to update a recipe card with a given recipe name
     void updateRecipeCard(RecipeCardWidget* card, const QString& recipeName);
+
+    // Method to update the image of a recipe card with a given image path
     void updateRecipeCardImage(RecipeCardWidget* card, const QString& imagePath);
 };
 
